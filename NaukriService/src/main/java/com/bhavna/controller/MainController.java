0951de8d61b033acec1560d910b8544817a03134
SrikanthNanaboina <srikanth.nanaboina@bhavnacorp.com>
2022-03-15 14:22:24 +0530

    Naukri service

---
+++ NaukriService/src/main/java/com/bhavna/controller/MainController.java
@@ -0,0 +1,46 @@
+package com.bhavna.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.bhavna.modal.Employee;
+
+
+@RestController
+@RefreshScope
+public class MainController {
+	@Value("${domainName}")
+	private String domainName;
+	
+	@Autowired
+	RestTemplate restTemplate;
+ 
+	@GetMapping("/demo")
+	public String getDemo() {
+		return "Hello";
+	}
+	
+	@PostMapping("saveProfile")
+	public ResponseEntity<Employee> postDemo(@RequestBody Employee employee) {
+		HttpEntity<Employee> request = new HttpEntity<>(employee);
+		ResponseEntity<Employee> response = restTemplate
+		  .exchange("http://localhost:8082/api-employee/save/employee", HttpMethod.POST, request, Employee.class);
+		return response;
+	}
+	
+	@GetMapping("getProfile/{username}")
+	public ResponseEntity<Employee> getStudentList(@PathVariable String username) {
+		return restTemplate.getForEntity("http://localhost:8082/api-employee/get/profile/"+username+domainName,Employee.class);
+	}
+}
---
+++ NaukriService/src/main/java/com/bhavna/modal/Employee.java
@@ -0,0 +1,44 @@
+package com.bhavna.modal;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+	private String username;
+	private String name;
+	private Integer age;
+	private String email;
+	private String gender;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
